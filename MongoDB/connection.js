6aa5ebd8ef9ebf50dda8d3d487de4aad33e3a5a7
gurpreetsingh-logiciel <115771297+gurpreetsingh-logiciel@users.noreply.github.com>
2023-11-13 21:00:44 +0530

    DB Connection Implemented.

--- MongoDB/connection.js
+++ MongoDB/connection.js
@@ -1,5 +1,8 @@
+// ENV VARIABLE
+require('dotenv').config();
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://prince_4547:prince123@cluster0.qbnwag1.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.CONNECTION_USER_NAME}:${process.env.CONNECTION_PASSWORD}@cluster0.qbnwag1.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,12 +20,14 @@ const createConnection = async () => {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-
+        
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        const data = await client.db("admin").command({ ping: 1 });
 
-        return client;
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        return data;
+    } catch (error) {
+        console.log(error);
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// Packages
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// ENV VARIABLE
+require('dotenv').config();
+
+// Helper
+const app = express();
+
+// DB Connection
 const CONNECTION = require('./MongoDB/connection.js');
 
-CONNECTION.createConnection();
+const Mongo_DB_Connection = CONNECTION.createConnection();
+
+// App Listen at port
+const port = process.env.PORT;
+
+// Parse JSON requests
+app.use(bodyParser.json());
+
+// Handle POST requests to /api/post
+app.post('/api/post', (req, res) => {
+    // Access the posted data in the request body
+    const postData = req.body;
+
+    // Do something with the data (e.g., save it to a database)
+    console.log('Received POST data:', postData);
+
+    // Send a response
+    res.json({ message: 'Data received successfully!' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
